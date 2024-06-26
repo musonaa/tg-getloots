1eeb013881dc85dc92a233801c2037abf1d6ef95
musonaa <angelika.skyler@ukr.net>
2024-06-26 10:44:12 -0500

    initial

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import './App.css';
+const tg = window.Telegram.WebApp;
+function App() {
+
+  useEffect(() => {
+    tg.ready();
+  }, [] )
+
+  const onClose = () => {
+    tg.close()
+  }
+  return (
+    <div className="App">
+      GetLoots
+      <button onClick={onClose}>Close</button>
+    </div>
+  );
+}
+
+export default App;

27f9c45d706804173198ea5c4c3f060f9558df0d
musonaa <angelika.skyler@ukr.net>
2024-06-26 13:26:34 -0500

    init

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,18 +1,35 @@
+// import React from 'react';
+// // import './components/header/header.css'; 
+// // import { useTelegram } from '../../hooks/useTelegram';
+// import './header.css'; // Adjusted path
+// import { useTelegram } from '../../hooks/useTelegram'; // Adjusted path
+
+// const Header = () => {
+//     const {user, onClose} = useTelegram();
+
+//     return (
+//         <div className={'header'}>
+//             <button onClick={onClose}>Закрыть</button>
+//             <span className={'username'}>
+//                 {user?.username}
+//             </span>
+//         </div>   
+//     )
+// }
+
 import React from 'react';
-// import './components/header/header.css'; 
-// import { useTelegram } from '../../hooks/useTelegram';
-import './header.css'; // Adjusted path
-import { useTelegram } from '../../hooks/useTelegram'; // Adjusted path
+import { useTelegram } from '../../hooks/useTelegram';
+import './header.css'; 
 
 const Header = () => {
-    const {user, onClose} = useTelegram();
+    const { user, onClose } = useTelegram();
 
     return (
-        <div className={'header'}>
+        <div className='header'>
             <button onClick={onClose}>Закрыть</button>
-            <span className={'username'}>
+            <span className='username'>
                 {user?.username}
             </span>
         </div>   
-    )
+    );
 }

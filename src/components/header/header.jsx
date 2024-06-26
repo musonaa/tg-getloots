90f677b6e2310ecbbffbacbce5cda4008c33e6db
musonaa <angelika.skyler@ukr.net>
2024-06-26 13:00:12 -0500

    init

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { useTelegram } from 'src/hooks/useTelegram';
+import { useTelegram } from './hooks/useTelegram';
 
 const Header = () => {
     const {user, onClose} = useTelegram();

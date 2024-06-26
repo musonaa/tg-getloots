6b13d94b25b748a1ab4de910fb0d82bc1740b7c5
musonaa <angelika.skyler@ukr.net>
2024-06-26 13:12:50 -0500

    init

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 import React, { useEffect } from 'react';
-import { useTelegram } from './hooks/useTelegram';
-import Header from "./components/header/header"
+import {useTelegram} from './hooks/useTelegram';
+import Header from "./components/header/header";
 
 const tg = window.Telegram.WebApp;
 function App() {

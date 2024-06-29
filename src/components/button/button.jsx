9ffe28a3bfdf85ab1f5e03fa5c11b529207d56a5
musonaa <angelika.skyler@ukr.net>
2024-06-29 01:21:49 -0500

    updates

--- src/components/button/button.jsx
+++ src/components/button/button.jsx
@@ -1,21 +1,21 @@
-// import React from 'react';
-// import './button.css'; 
-
-// const Button = (props) => {
-//     return (
-//         <button{...props} className={'button' + props.className}/>
-//     )
-// }
-
-// export default Button;
 import React from 'react';
 import './button.css'; 
 
 const Button = (props) => {
-    const { className, ...rest } = props;
     return (
-        <button {...rest} className={`button ${className || ''}`} />
+        <button{...props} className={'button' + props.className}/>
     )
 }
 
 export default Button;
+// import React from 'react';
+// import './button.css'; 
+
+// const Button = (props) => {
+//     const { className, ...rest } = props;
+//     return (
+//         <button {...rest} className={`button ${className || ''}`} />
+//     )
+// }
+
+// export default Button;

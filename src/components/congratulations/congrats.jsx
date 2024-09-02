75394840cbcf357b5d7112881444a9ab859b4a87
musonaa <angelika.skyler@ukr.net>
2024-09-01 23:06:09 -0500

    i

--- src/components/congratulations/congrats.jsx
+++ src/components/congratulations/congrats.jsx
@@ -8,7 +8,8 @@ const Congratulations = () => {
             <h1>Ваш заказ успешно оформлен!</h1>
             <p>Ожидайте пока @lLojin свяжется с вами. Спасибо что воспользовались нашим сервисом!</p>
             <div className="gif-section">
-                <img src="https://media.tenor.com/WZvgDcNnWQQAAAAC/kawaii-cute.gif" alt="Thank YOU" className="tu-gif" />
+                {/* <img src="https://media.tenor.com/WZvgDcNnWQQAAAAC/kawaii-cute.gif" alt="Thank YOU" className="tu-gif" /> */}
+                <div class="tenor-gif-embed" data-postid="23743858" data-share-method="host" data-aspect-ratio="1" data-width="100%"><a href="https://tenor.com/view/kawaii-cute-peach-cat-gif-23743858">Kawaii Cute GIF</a>from <a href="https://tenor.com/search/kawaii-gifs">Kawaii GIFs</a></div> <script type="text/javascript" async src="https://tenor.com/embed.js"></script>
             </div>
         </div>
     );

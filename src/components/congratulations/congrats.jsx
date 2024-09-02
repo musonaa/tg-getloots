18c95c853a07fa5da2d8137348a17f5f5c85b84a
musonaa <angelika.skyler@ukr.net>
2024-09-01 20:38:33 -0500

    i

--- src/components/congratulations/congrats.jsx
+++ src/components/congratulations/congrats.jsx
@@ -4,8 +4,8 @@ import './congrats.css';
 const Congratulations = () => {
     return (
         <div className="congratulations-container">
-            <h1>Congratulations!</h1>
-            <p>Your order has been placed successfully.</p>
+            <h1>Спасибо что воспользовались нашим сервисом!</h1>
+            <p>Ваш заказ успешно размещен.</p>
         </div>
     );
 };

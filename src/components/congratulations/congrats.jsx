a671647ad76b94f0385aa6e2521c282da2d0554b
musonaa <angelika.skyler@ukr.net>
2024-09-01 23:57:18 -0500

    i

--- src/components/congratulations/congrats.jsx
+++ src/components/congratulations/congrats.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import './congrats.css';
+import tuGif from '/images/kawaii-cute.gif';
 
 const Congratulations = () => {
     return (
@@ -7,7 +8,8 @@ const Congratulations = () => {
             
             <h1>Ваш заказ успешно оформлен!</h1>
             <p>Ожидайте пока @lLojin свяжется с вами. Спасибо что воспользовались нашим сервисом!</p>
-            <img src="/images/kawaii-cute.gif" alt="Thank You" className="tu-gif" />
+            {/* <img src="/images/kawaii-cute.gif" alt="Thank You" className="tu-gif" /> */}
+            <img src="/images/trahs.png"/>
         </div>
     );
 };

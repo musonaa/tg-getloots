382f4037e589e6712e2e147988369f0498274179
musonaa <angelika.skyler@ukr.net>
2024-09-02 00:42:50 -0500

    i

--- src/components/congratulations/congrats.jsx
+++ src/components/congratulations/congrats.jsx
@@ -10,7 +10,7 @@ const Congratulations = () => {
             <p>Ожидайте пока @lLojin свяжется с вами. Спасибо что воспользовались нашим сервисом!</p>
             {/* <img src="/images/kawaii-cute.gif" alt="Thank You" className="tu-gif" /> */}
             <div>
-                <img src="/images/trahs.png" />
+                <img src="/images/trash.png" />
             </div>
             
             {/* <div style="width:100%;height:0;padding-bottom:75%;position:relative;"><iframe src="https://giphy.com/embed/LHZyixOnHwDDy" width="100%" height="100%" style="position:absolute" frameBorder="0" class="giphy-embed" allowFullScreen></iframe></div><p><a href="https://giphy.com/gifs/computer-working-cat-LHZyixOnHwDDy">via GIPHY</a></p> */}

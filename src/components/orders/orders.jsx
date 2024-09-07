9eded2413e73f000f2b1ef1b6901c10ccfeba70e
musonaa <angelika.skyler@ukr.net>
2024-09-07 11:27:40 -0500

    i

--- src/components/orders/orders.jsx
+++ src/components/orders/orders.jsx
@@ -54,7 +54,8 @@ const OrdersComponent = () => {
         const fetchOrders = async () => {
             try {
                 // const response = await fetch('http://127.0.0.1:3001/orders');
-                const response = await fetch('https://46.101.117.21:25742/orders');
+                //25742
+                const response = await fetch('https://46.101.117.21:80/orders');
                 const data = await response.json();
                 setOrders(data);
             } catch (error) {
